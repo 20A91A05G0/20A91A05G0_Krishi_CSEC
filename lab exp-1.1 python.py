116ec5242ba66caa5fec332c76fdf4e2e48f1fce
20A91A05G0 <84382254+20A91A05G0@users.noreply.github.com>
2021-05-20 10:43:57 +0530

    Add files via upload

---
+++ lab exp-1.1 python.py
@@ -0,0 +1,15 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("enter first number:"))
+enter first number:2
+>>> b=int(input("enter second number:"))
+enter second number:3
+>>> c=int(input("enter third number:"))
+enter third number:4
+>>> total=a+b+c
+>>> print(total)
+9
+>>> average=total/3
+>>> print(average)
+3.0
+>>> 
