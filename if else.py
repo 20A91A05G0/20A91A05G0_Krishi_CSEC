0affd661f88cf14d18e80eb2f8f04c580744197d
20A91A05G0 <84382254+20A91A05G0@users.noreply.github.com>
2021-05-24 12:33:00 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,12 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input())
+987
+>>> if a%2==1:
+	print('a is odd')
+else:
+	print('a is even')
+
+	
+a is odd
+>>> 
